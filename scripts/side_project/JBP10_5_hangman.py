9ab1aef9f28f826e2a93517f629bc7091ffd535b
AkaruiYami <shahrullfytri1@gmail.com>
2021-09-29 15:21:00 +0800

    fix bugs and typo

--- scripts/side_project/JBP10_5_hangman.py
+++ scripts/side_project/JBP10_5_hangman.py
@@ -1,3 +1,22 @@
+"""
+Game hangman dalam terminal.
+
+Pengguna akan teka huruf yang mungkin ada dalam perkataan misteri.
+Sekiranya huruf yang pengguna teka itu ada, maka huruf tersebut akan di tunjukkan
+dalam perkataan misteri tersebut.
+Sekiranya huruf yang pengguna teka itu salah, maka cubaan penggunaan akan berkurang.
+Pengguna akan tewas apabila semua cubaan sudah digunakan.
+
+start:
+    _ _ _ _ _   _ _ _
+
+input:
+    a
+
+output:
+    Betul!
+    _ a _ a _   _ a _
+"""
 from random import choice
 
 animal_list = (
@@ -14,49 +33,42 @@ animal_list = (
     "ular sawa",
 )
 
-
-def print_word(word_target, letter_guessed):
-    for c in word_target:
-        if c in letter_guessed or c == " ":
-            print(c, end="")
+def print_board(word, letter_to_guess):
+    for c in word:
+        if c in letter_to_guess:
+            print(" _ ", end="")
         else:
-            print("_", end="")
+            print(f" {c} ".upper(), end="")
     print()
 
-
 if __name__ == "__main__":
-    print("Selamat Datang Ke Permainan Teka Kata!")
-    print("\nAnda perlu meneka huruf yang mungkin ada dalam kata berikut.")
-    print("Anda hanya mempunya 3 cubaan.")
-
     random_word = choice(animal_list)
-    letter_to_guesse = set([c for c in random_word.replace(" ", "")])
+    letter_to_guess = set(random_word.replace(" ", ""))
     letter_guessed = set()
     n_trial = 3
     
-    print(letter_to_guesse)
     while True:
-        print_word(random_word, letter_guessed)
+        print_board(random_word, letter_to_guess)
         guess = input("$  ").lower()
 
         if guess in letter_guessed:
-            print(f"Huruf `{guess}` sudah diteka! Cuba huruf lain.")
+            print(f"Huruf `{guess}` sudah di teka!")
             continue
 
-        if guess in letter_to_guesse:
-            letter_to_guesse.remove(guess)
+        if guess in letter_to_guess:
+            letter_to_guess.remove(guess)
             print("Betul!")
         else:
             n_trial -= 1
-            print(f"Salah! Cubaan anda tinggal {n_trial} sahaja lagi.")
+            if n_trial == 0:
+                print("Anda sudah tewas!")
+                break
+            print(f"Salah! Anda hanya ada {n_trial} cubaan sahaja lagi.")
 
-        if len(letter_to_guesse) == 0:
-            print("Tahniah! Anda Menang!")
-            print(random_word)
-            break
-
-        if n_trial == 0:
-            print("Anda sudah tewas!")
+        if len(letter_to_guess) == 0:
+            print("Anda sudah menang!")
+            print(f"Perkataan Mister: {random_word.upper()}")
             break
 
         letter_guessed.add(guess)
+
