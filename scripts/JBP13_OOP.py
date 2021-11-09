42e01c43d3a14f3d9de6045f15ac887be1e8f12f
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2021-11-09 18:23:58 +0800

    add notes

--- scripts/JBP13_OOP.py
+++ scripts/JBP13_OOP.py
@@ -1,19 +1,27 @@
-class Person:
-    rank = "No Rank Yet"
+# What is class, object and instance
+# Class is a blueprint that we use to create an object
+# Instance is an object of the class that we created
 
+# How to create class and instance
+class Person:
+    # Creating Class Variable
+    rank = "No Rank"
     def __init__(self, name, age):
+        # Creating Intance Variable
         self.name = name
         self.age = age
 
-    def greeting(self):
+    # Creating in class function
+    def greetings(self):
         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
 
-
+# Create child class where it parent is class Person
+# Child class will inherit class Person
 class Student(Person):
     def __init__(self, name, age, gender):
         super().__init__(name, age)
         self.gender = gender
 
 
-student1 = Student("AkaruiYami", 17, "male")
-student1.greeting()
+student_a = Student("AkaruiYami", 18, "Male")
+print(student_a.name)
